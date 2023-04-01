0f23cd9e169978247de3e0f4be14cb5fe155b5cd
Herisson Silva <herissonsilvahs@gmail.com>
2023-04-01 08:55:12 -0300

    feat: output terraform apply to hosts ansible file

--- ansible/n8n/outputs.tf
+++ ansible/n8n/outputs.tf
@@ -1,4 +1,15 @@
 output "n8nip" {
   description = "n8n ip"
-  value = digitalocean_droplet.n8ntest01.ipv4_address
+  value = digitalocean_droplet.n8n01.ipv4_address
+}
+
+resource "local_file" "tf_ansible_vars_file_new" {
+  content = <<-DOC
+    [n8n]
+    ${digitalocean_droplet.n8n01.ipv4_address}
+
+    [all:vars]
+    ansible_user = root
+    DOC
+  filename = "../hosts"
 }
